5ff4104c714a3f9ae5c9c921b2c4a08302cdf6d4
Antonio Mora <nmblotta@gmail.com>
2026-06-05 20:08:07 -0600

    fix respuestas.py being empty

---
+++ Lesson-1/respuestas.py
@@ -0,0 +1,20 @@
+#Aquí están las respuestas de los ejercicios si las necesitan.
+#Solo usar si fracasan terriblemente, pero preferible no verlas a no ser que sea para revisar.
+
+#Ejercicio 1:
+
+# CODIGO:
+# print(type(entero))
+# print(type(entero_negativo))
+# print(type(decimal))
+# print(type(texto))
+# print(type(booleano))
+# print(type(nulo))
+
+# SALIDA/OUTPUT:
+# <class 'int'>
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'bool'>
+# <class 'NoneType'>
