2222813540b797137d32da3e9fb71c4abad7d693
Antonio Mora <nmblotta@gmail.com>
2026-06-05 20:42:27 -0600

    update respuestas.py

--- Lesson-1/respuestas.py
+++ Lesson-1/respuestas.py
@@ -18,3 +18,48 @@
 # <class 'str'>
 # <class 'bool'>
 # <class 'NoneType'>
+
+
+#Ejercicio 2:
+
+# CODIGO:
+# print(4567 % 2 == 0)
+
+# SALIDA/OUTPUT:
+# False
+# (da False, o sea que el resto de dividir 4567 entre 2 NO es 0,
+#  entonces 4567 es impar)
+
+
+#Ejercicio 3:
+
+# CODIGO:
+# numero1 = int(input("Escribí un número: "))
+# numero2 = int(input("Escribí otro número: "))
+# print(numero1 + numero2)
+
+# SALIDA/OUTPUT (suponiendo que el usuario escribe 2 y luego 3):
+# Escribí un número: 2
+# Escribí otro número: 3
+# 5
+#
+# Ojo: el int() de cada input es lo importante. Si lo dejan sin convertir,
+# "2" + "3" daría "23" (texto pegado) en vez de 5.
+
+
+#Ejercicio 4:
+
+# CODIGO:
+# edad = int(input("¿Cuántos años tenés? "))
+# if edad >= 18:
+#     print("Puede entrar")
+# else:
+#     print("No puede entrar")
+
+# SALIDA/OUTPUT (suponiendo que el usuario escribe 20):
+# ¿Cuántos años tenés? 20
+# Puede entrar
+#
+# Y si escribe 15:
+# ¿Cuántos años tenés? 15
+# No puede entrar
